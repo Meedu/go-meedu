59b70bf1be091881619ec5055d30b4be51102024
Xiaoteng <616896861@qq.com>
2019-04-13 23:47:21 +0800

    add:notify && intro

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 ## MeEdu 插件安装工具
 
 因为php在fpm跑composer命令稳定性太差，所以使用go提供api服务来安装composer的依赖，从而使得插件安装更加方便。
+
+### Usage
+
+```
+./go-meedu -address=0.0.0.0:8089
+```
+
+> 建议配置 supervisor 等进程管理软件一起使用。
+
+### 服务
+
+对外提供 `/install` API，参数如下：
+
+| 参数 | 类型 | 说明 |
+| --- | --- | --- |
+| `php` | `string` | php执行路径 | 
+| `composer` | `string` | composer路径 |
+| `action` | `string` | composer行为，es:require,remove |
+| `pkg` | `string` | 包名，如：monolog/monolog=dev-master |
+| `dir` | `string` | 命令执行路径，一般为meedu所在根目录 |
+| `addons` | `string` | 需要安装依赖的插件，用于回调 |
+| `notify` | `string` | 回调URL，用于通知meedu的插件依赖安装状态 |
--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"sync"
 )
+
+var mu sync.Mutex
 
 func main() {
 	var address = flag.String("address", "0.0.0.0:8089", "please input listen address.")
@@ -18,17 +21,28 @@ func main() {
 		action := form.Get("action")
 		pkg := form.Get("pkg")
 		dir := form.Get("dir")
+		addons := form.Get("addons")
+		notify := form.Get("notify")
 		if php == "" || composer == "" || action == "" || pkg == "" {
 			fmt.Println("params error")
 			return
 		}
 		// 执行命令
 		go func() {
+			mu.Lock()
 			fmt.Printf("prepare run [%s %s %s %s] command\n", php, composer, action, pkg)
 			cmd := exec.Command(php, composer, action, pkg)
 			cmd.Dir = dir
-			output, _ := cmd.CombinedOutput()
-			fmt.Printf("%s", output)
+			output, err := cmd.CombinedOutput()
+			fmt.Printf("%s %v", output)
+			mu.Unlock()
+
+			// 回调
+			status := "success"
+			if err != nil {
+				status = "fail"
+			}
+			_, _ = http.Get(notify + "?addons=" + addons + "&status=" + status)
 		}()
 	})
 	fmt.Printf("listen:%s\n", *address)
